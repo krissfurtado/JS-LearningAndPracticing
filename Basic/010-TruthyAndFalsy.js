4166cb94384ab38d9e86e15cc0250a5e0b478c9f
krissfutado <krissfurtado@gmail.com>
2023-05-03 17:13:50 -0300

    add 010 & 011

--- Basic/010-TruthyAndFalsy.js
+++ Basic/010-TruthyAndFalsy.js
@@ -2,3 +2,18 @@
 https://developer.mozila.org/en-US/docs/Glossary/Falsy
 https://developer.mozilla.org/en-US/docs/Glossary/Truthy
 */
+//falsy
+console.log(`
+    0: ${0 ? true : false}
+    "": ${"" ? true : false}
+    null: ${ null ? true : false}
+    undefined: ${undefined ? true : false}
+`)
+
+
+//truthy
+console.log(`
+    1: ${ 1 ? true : false}
+    "0": ${"0" ? true : false}
+    "ABC": ${ "ABC" ? true : false}
+`)
---
+++ Basic/011-AndOr.js
@@ -0,0 +1,17 @@
+//OR
+console.log(`OR`)
+console.log(`false || false: `,false || false)
+console.log(`false || true: `,false || true)
+console.log(`true || true: `,true || true)
+console.log(`true || false: `,true || false)
+
+//AND
+console.log(`\nAND`)
+console.log(`true && true: `,true && true)
+console.log(`true && false: `,true && false)
+console.log(`false && true: `,false && true)
+console.log(`false && false: `,false && false)
+
+//MIX
+console.log(`\nMIX`)
+console.log(`false || false && (true && false): `,false || false && (true && true))
