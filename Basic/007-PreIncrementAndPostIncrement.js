715e576d99f1a5c7a99ad1b5a29b84bea9ed9bc3
krissfutado <krissfurtado@gmail.com>
2023-05-03 14:12:54 -0300

    file 007 (Increme) and 000-index (import js files)

---
+++ Basic/007-PreIncrementAndPostIncrement.js
@@ -0,0 +1,9 @@
+/*
+Pre-increment ++i increments the value of i and evaluates to the new incremented value. 
+
+Post-increment i++ increments the value of i and evaluates to the original non-incremented value.
+*/
+
+i = 10;
+//console.log(++i)
+console.log(i++)
