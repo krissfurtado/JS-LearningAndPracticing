54f8479eb9a33b7685ffd52b27661a5b664245ee
krissfutado <krissfurtado@gmail.com>
2023-05-03 15:46:15 -0300

    file 007 (Pre/Post increment ) and 008 about assig

---
+++ Basic/007-PreAndPostIncrementAndDecrement.js
@@ -0,0 +1,18 @@
+/*
+Pre-increment ++i increments the value of i and evaluates to the new incremented value. 
+
+Post-increment i++ increments the value of i and evaluates to the original non-incremented value.
+*/
+
+i = 10;
+j = 20;
+k = 2;
+
+//pre-increment
+console.log(`i= ${i} \n++i= ${++i} (first increments and then shows) \n--------------------------------------------`) //first increments and then shows
+
+//post-increment
+console.log(`j= ${j} \nj++= ${j++} (firt shows and then increments)    \n--------------------------------------------`) //firt shows and then increments
+
+//pre-decrement
+console.log(`i= ${i} \n--i= ${--i} (first decrements and then shows) \n--------------------------------------------`) //first increments and then shows
--- Basic/007-PreIncrementAndPostIncrement.js
+++
@@ -1,9 +0,0 @@
-/*
-Pre-increment ++i increments the value of i and evaluates to the new incremented value. 
-
-Post-increment i++ increments the value of i and evaluates to the original non-incremented value.
-*/
-
-i = 10;
-//console.log(++i)
-console.log(i++)
