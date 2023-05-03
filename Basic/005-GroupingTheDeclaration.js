3cd52d52f26f7af5f9eb663f8b2138c0902fee38
krissfutado <krissfurtado@gmail.com>
2023-05-03 15:49:49 -0300

    rename file 005 and 006

---
+++ Basic/005-GroupingTheDeclaration.js
@@ -0,0 +1,2 @@
+name = " kris", age = 38, street = "st7";
+console.log(name, "\n", age, "\n", street)
---
+++ Basic/006-ConcatenationAndInterpolation.js
@@ -0,0 +1,6 @@
+name = " kris", age = 38, street = "st7";
+
+//concatenating:
+console.log("My name is" + name + " and I am", age, "years old and I live in " + street)
+//interpolating
+console.log(`My name is${name} and I am ${age} years old and I live in ${street}`)
