71b9ec05e26b66fb065f68a722fcb63f00999a3a
krissfutado <krissfurtado@gmail.com>
2023-05-05 08:00:41 -0300

    001-Function

---
+++ 02-Advanced/001-Function.js
@@ -0,0 +1,22 @@
+//function to say hello
+var name = "Jhon"
+
+
+function hello (xxx){
+    console.log(`Hello ${xxx}`)
+}
+
+hello(name)
+
+function sumTwoNumbers(x,y) {
+    console.log(`${x} + ${y}= ${x+y}`)
+}
+
+sumTwoNumbers(10,5)
+
+//using return
+function mutiplyBy(a,b){
+    return `${a} x ${b}= ${a*b}`
+}
+
+console.log(mutiplyBy(5,10))
